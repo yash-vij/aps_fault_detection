3b34a53a4f77035c836b75248f07f92675a6ae19
yash-vij <yashvij1996@gmail.com>
2022-11-29 23:05:34 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import find_packages,setup
 
-def get_requirements():
-    pymongo
-    pandas
+from typing import List
+
+REQUIREMENT_FILE = "requirements.txt"
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements()-> List[str]:
+    with open(REQUIREMENT_FILE) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return  requirement_list
+
 
 setup(
     name="sensor",
@@ -10,5 +20,5 @@ setup(
     author="yash",
     author_email="yashvij1996@gmail.com",
     packages=find_packages(),
-    istall_requires = get_requirements(),
+    istall_requires = get_requirements()
 )
