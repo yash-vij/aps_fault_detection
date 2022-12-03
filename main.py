faeb3bcac1694165f01c7f74b8112040d7fcf2f2
yash-vij <yashvij1996@gmail.com>
2022-12-03 14:38:23 +0530

    New Updates

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
-import pymongo
+import sys
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb+srv://yashvij:<password>@cluster0.wnq7l.mongodb.net/?retryWrites=true&w=majority")
+from sensor.logger import  logging
+from sensor.execption import SensorException
 
-# Database Name
-dataBase = client["neurolabDB"]
+def test_logger_and_exception():
+     try:
+          result = 3/0
+          print(result)
+     except Exception as e :
+          raise SensorException(e, sys)
 
-# Collection  Name
-collection = dataBase['Products']
 
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
-
-# Insert above records in the collection
-rec = collection.insert_one(d)
-
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
-
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
-     
+if __name__ == "__main__":
+     try:
+          test_logger_and_exception()
+     except Exception as e:
+          print(e)
